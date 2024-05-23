7d0fb8a482118bb8873b17e6e8f9a62e9fa305b7
Jerry <Jerrylin2488@gmail.com>
2024-05-22 21:33:53 -0400

    Update Navbar.js

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -2,14 +2,14 @@ import React from 'react';
 
 function Navbar() {
     return (
-        <nav className="bg-gray-800 text-white p-4">
-            <div className="container mx-auto flex justify-between items-center">
-                <h1 className="text-lg font-bold">Spudy</h1>
+        <nav className="bg-gray-800 text-white p-4 fixed top-0 left-0 right-0 z-10">
+            <div className="container mx-auto flex justify-between items-center w-full">
+                <h1 className="text-3xl font-bold">Spudy</h1>  
                 <div>
-                    <a href="/" className="px-4 hover:text-gray-300">Home</a>
-                    <a href="/about" className="px-4 hover:text-gray-300">About</a>
-                    <a href="/features" className="px-4 hover:text-gray-300">Features</a>
-                    <a href="/contact" className="px-4 hover:text-gray-300">Contact</a>
+                    <a href="/" className="px-4 hover:text-gray-300 text-lg">Home</a>  
+                    <a href="/about" className="px-4 hover:text-gray-300 text-lg">About</a>  
+                    <a href="/login" className="px-4 hover:text-gray-300 text-lg">Log in</a> 
+                    <a href="/contact" className="px-4 hover:text-gray-300 text-lg">Contact</a> 
                 </div>
             </div>
         </nav>
