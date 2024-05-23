5d3c9e90d98e7d00b0e018cef6e9565ba34c0f7d
Jerry <Jerrylin2488@gmail.com>
2024-05-23 15:33:08 -0400

    Initial commit

---
+++ client/src/pages/Signup.js
@@ -0,0 +1,80 @@
+import React, { useState } from 'react';
+import './Signup.css';
+
+function Signup() {
+    const [userData, setUserData] = useState({
+        username: '',
+        email: '',
+        password: '',
+        confirmPassword: ''
+    });
+
+    const handleChange = (event) => {
+        const { name, value } = event.target;
+        setUserData(prevState => ({
+            ...prevState,
+            [name]: value
+        }));
+    };
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        // Handle the signup logic here
+        console.log('User Data:', userData);
+    };
+
+    return (
+        <div className="signup-container">
+            <form onSubmit={handleSubmit} className="signup-form">
+                <h2>Sign Up</h2>
+                <div className="input-group">
+                    <label htmlFor="username">Username:</label>
+                    <input
+                        type="text"
+                        id="username"
+                        name="username"
+                        value={userData.username}
+                        onChange={handleChange}
+                        required
+                    />
+                </div>
+                <div className="input-group">
+                    <label htmlFor="email">Email:</label>
+                    <input
+                        type="email"
+                        id="email"
+                        name="email"
+                        value={userData.email}
+                        onChange={handleChange}
+                        required
+                    />
+                </div>
+                <div className="input-group">
+                    <label htmlFor="password">Password:</label>
+                    <input
+                        type="password"
+                        id="password"
+                        name="password"
+                        value={userData.password}
+                        onChange={handleChange}
+                        required
+                    />
+                </div>
+                <div className="input-group">
+                    <label htmlFor="confirmPassword">Confirm Password:</label>
+                    <input
+                        type="password"
+                        id="confirmPassword"
+                        name="confirmPassword"
+                        value={userData.confirmPassword}
+                        onChange={handleChange}
+                        required
+                    />
+                </div>
+                <button type="submit" className="signup-button">Sign Up</button>
+            </form>
+        </div>
+    );
+}
+
+export default Signup;
