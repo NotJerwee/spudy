a3d57eb7ae428b5af86b601d5b6ee69c746a0afc
Jerry <Jerrylin2488@gmail.com>
2024-05-22 20:32:32 -0400

    Update Home.js

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -2,8 +2,8 @@ import React from 'react'
 
 function Home() {
 	return (
-		<div className='w-full h-screen bg-[#1a1a1a] text-white flex justify-center items-center'>
-			<h2 className='text-3xl'>HOME</h2>
+		<div className='w-full bg-[#1a1a1a] text-white flex justify-center items-center'>
+			<h2 className='text-3xl'>HOME</h2>	
 		</div>
 	)
 }
